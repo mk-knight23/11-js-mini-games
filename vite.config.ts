651155cd62bf38b9cbc3a7bd5e113ced748e7ce0
mk.knight23 <mk.knight23@example.com>
2026-01-21 11:48:41 +0530

    chore: unify vite config and build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: './',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                p1: '01-background-color-changer/index.html',
+                p2: '02-popup-notification/index.html',
+                p3: '03-countdown-timer/index.html',
+                p4: '04-dark-mode-toggle/index.html',
+                p5: '05-calculator/index.html',
+            },
+        },
+    },
 })
